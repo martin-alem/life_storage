1d4281a5b91697f3c58bec2347c014f33e5bf7a3
Martin Alemajoh <alemajohmartin@gmail.com>
2022-01-22 17:57:16 -0500

    Implemented the functionality of uploading the images to cloudinary, also implemented views to handle the submission of the uploaded media and finally implemented a view to handle the search functionality

--- life_storage/settings.py
+++ life_storage/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tws#z%zw_tnd=r^h!%47+qw68hltm47a3qrc0!!($&p78*x)4#'
+SECRET_KEY = ""
+#'django-insecure-tws#z%zw_tnd=r^h!%47+qw68hltm47a3qrc0!!($&p78*x)4#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
